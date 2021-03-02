569b32c2cd41aa6219576f8d052c2e10c77b90b7
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-02 16:01:08 +0100

    inserite condizioni % sia per 5 che per tre e altri numeri

--- js/script.js
+++ js/script.js
@@ -6,12 +6,21 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
 var fizzBuzz = [];
 //Definisco il ciclo di FizzBuzz
 for ( var i = 1; i <= 100; i++) {
+  //Inserisco la condizione per gli i divisibili sia per 5 che per 3
+  if ( i % 15 == 0 ) {
+    console.log('FizzBuzz');
+  }
 //Sostituisco gli i divisibili per 3 con la stringa Fizz
-  if ( i % 3 == 0 ) {
+ else if ( i % 3 == 0 ) {
    console.log('Fizz');
- } //Sostituisco gli i divisibili per 5 con la stringa Buzz
+ }
+ //Sostituisco gli i divisibili per 5 con la stringa Buzz
   else if ( i % 5 == 0 ) {
     console.log('Buzz');
+  }
+  //Inserisco la condizione per mostrare gli i non divisibili per 3 o per 5
+  else {
+    console.log(i);
   }
 
 }
